5420c6070cccdc74e39069ba89f75d3c7c94753e
MarukiIroha <derek15602919761@gmail.com>
2023-07-17 15:06:11 +0800

    Converting Celsius to Fahrenheit and vice versa.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,4 +1,6 @@
+#Constant
 LENGTH = 5
+
 
 def main():
     password = get_password()
@@ -6,10 +8,12 @@ def main():
 
 
 def print_asterisks(password):
+    """print *"""
     print("*" * len(password))
 
 
 def get_password():
+    """get the password"""
     password = input("")
     while len(password) < LENGTH:
         print("Invalid length")
