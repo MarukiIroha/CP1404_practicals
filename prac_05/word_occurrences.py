ae8bb2c234a6e6137a85b8ade7ddc6a54710c60b
MarukiIroha <derek15602919761@gmail.com>
2023-08-13 04:36:17 +0800

    Complete prac 5 word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,13 +4,13 @@ Estimate: 60 minutes
 Actual:   40.5 minutes
 """
 total_words = {}
-max1 = 0
+max_length = 0
 text = input("Text: ").split()
 for word in text:
         total_words[word] = total_words.get(word, 0) + 1
 for i in total_words:
-    if len(i) > max1:
-        max1 = len(i)
+    if len(i) > max_length:
+        max_length = len(i)
 for key in sorted(total_words):
-    thing, width, other_thing = key, max1, total_words[key]
+    thing, width, other_thing = key, max_length, total_words[key]
     print(f"{thing:{width}} : {other_thing}")
